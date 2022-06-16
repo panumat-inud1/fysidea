1d75a07c5ca3002896c84ac591802ecf0be5532a
Panumat inud <banzwingzaa@hotmail.com>
2022-06-17 01:39:48 +0700

    5

--- src/lib/game/component/index.ts
+++ src/lib/game/component/index.ts
@@ -2,4 +2,6 @@ import HaruCoin from "./coins/HaruCoin.svelte";
 import HoshiCoin from "./coins/HoshiCoin.svelte";
 import YutaCoin from "./coins/YutaCoin.svelte";
 import Profile from "./profile/Profile.svelte";
-export { HaruCoin, HoshiCoin, YutaCoin, Profile }
+import Owner from "./market/owner/Owner.svelte";
+import BtnOwner from "./market/owner/BtnOwner.svelte";
+export { HaruCoin, HoshiCoin, YutaCoin, Profile, Owner, BtnOwner }
